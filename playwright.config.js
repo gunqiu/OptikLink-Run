806afdf2125445e69eb298449b71c5e7eb787206
gunqiu <myu88426@gmail.com>
2026-05-29 23:27:04 +0800

    Update playwright.config.js

--- playwright.config.js
+++ playwright.config.js
@@ -2,14 +2,13 @@
 const { defineConfig } = require('@playwright/test');
 
 module.exports = defineConfig({
-    testDir: './tests',
-    timeout: 120000,
-    retries: 0,
-    workers: 1,
-    reporter: [['list']],
-    use: {
-        headless: true,
-        screenshot: 'only-on-failure',
-        video: 'off',
-    },
+  testDir: './tests',
+  timeout: 120000,
+  retries: 2,
+  reporter: [['list']],
+  use: {
+    screenshot: 'only-on-failure',
+    trace: 'retain-on-failure',
+  },
+  outputDir: './test-results',
 });
